c4227833560e72257feeb758bbf721518d8705cd
GitLab9 <bakary.diakite@latoile.em-lyon.com>
2025-05-21 16:43:01 +0200

    j

---
+++ script.js
@@ -0,0 +1,71 @@
+function getData() {
+   fetch('data.json')
+     .then((response) => {
+       if (!response.ok) {
+         throw new Error('Network response was not ok');
+       }
+       return response.json();
+     })
+     .then((datas) => {
+       // Traitez les données comme vous le souhaitez
+       console.log('Données récupérées du fichier JSON :', datas);
+       /// ON ECRIT LE CODE ICI ! 
+
+      //.1 Créer une balise main et section est mettre les donnes du json dessus
+
+                // 1.0 On a créé une balise main et dans cette balise on mes une balise section avec des classe 
+      
+      let sectionMain = document.querySelector('section')
+      sectionMain.className = 'box';
+
+                // 1.1 Mettre des element dans la balise section avec le fichier json 
+      let video = document.querySelector('video');
+      let h1 = document.querySelector('h1');
+      let h2 = document.querySelector('h2');
+      let a = document.querySelector('a');
+      a.className = 'boxbtn';
+      
+      h1.textContent = datas.journal.nomJournal;
+      h2.textContent = datas.journal.phraseAccroche;
+      a.textContent = datas.journal.texteAppelAction;
+      
+    
+      
+      
+      
+      
+
+
+     
+
+      sectionMain.appendChild(video);
+
+      
+      
+      
+
+      
+
+
+
+      
+      
+      
+
+
+      
+      
+
+      
+      
+
+      
+
+       /// FIN DU CODE
+     })
+     .catch((error) => console.error('Erreur lors de la lecture des données :', error));
+ }
+ 
+ getData();
+
+ ///ON écrit les fonctions ici
